9cfb0843dcb7f1233f1e82d72839e8731da52e3a
Lâm <vhlam1997@gmail.com>
2020-03-07 17:13:25 +0700

    Updates

---
+++ precache-manifest.e70f683add8f3fcf6979baa786f47e86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d45ca39a67f231e36afdd2f330cb5d",
+    "url": "/recommendation-system/index.html"
+  },
+  {
+    "revision": "e689fbf08271432a023e",
+    "url": "/recommendation-system/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc1090acdb9680b9a088",
+    "url": "/recommendation-system/static/js/2.db8326fa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recommendation-system/static/js/2.db8326fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e689fbf08271432a023e",
+    "url": "/recommendation-system/static/js/main.011d2125.chunk.js"
+  },
+  {
+    "revision": "140588a4e03740343a72",
+    "url": "/recommendation-system/static/js/runtime-main.884b1c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recommendation-system/static/media/logo.5d5d9eef.svg"
+  }
+]);
